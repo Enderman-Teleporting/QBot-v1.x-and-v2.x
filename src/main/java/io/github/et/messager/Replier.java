525e4c7c1a92aeeb162bcee8f9d0a75f0dee6d02
王昱蘅 <sherrikevin5143@gmail.com>
2024-07-24 12:39:17 +0800

    update chat

--- src/main/java/io/github/et/messager/Replier.java
+++ src/main/java/io/github/et/messager/Replier.java
@@ -8,15 +8,12 @@ import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.message.data.At;
-import net.mamoe.mirai.message.data.Face;
-import net.mamoe.mirai.message.data.MessageChain;
-import net.mamoe.mirai.message.data.MessageChainBuilder;
+import net.mamoe.mirai.message.data.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-import static io.github.et.tools.API.getApi;
+import io.github.et.tools.Client;
 
 @SuppressWarnings("unused")
 public class Replier extends SimpleListenerHost {
@@ -34,43 +31,13 @@ public class Replier extends SimpleListenerHost {
     @EventHandler
     public void groupTalk(GroupMessageEvent msgEvent) throws IOException, LoggerNotDeclaredException {
         if(msgEvent.getMessage().contains(new At(msgEvent.getBot().getId()))) {
-            String msg = msgEvent.getMessage().contentToString();
-            msg = msg.replace(" ", "%20");
-            String result = getApi("https://api.qingyunke.com/api.php?key=free&appid=0&msg=" + msg, "content");
-            result = result.replace("菲菲", msgEvent.getBot().getNick());
-            if (result.contains("{") && result.contains("}")) {
-                StringBuilder prev = new StringBuilder();
-                StringBuilder aft = new StringBuilder();
-                StringBuilder num = new StringBuilder();
-                char[] MsgArray = result.toCharArray();
-                for (int i = 0; MsgArray[i] != '{'; i++) {
-                    prev.append(MsgArray[i]);
-                }
-                for (int j = 1; j < MsgArray.length; j++) {
-                    if (MsgArray[j - 1] == ':') {
-                        for (int k = j; MsgArray[k + 1] != '}'; k++) {
-                            num.append(MsgArray[k]);
-                        }
-                    }
-                    if (MsgArray[j - 1] == '}') {
-                        for (int l = j; l < MsgArray.length; l++) {
-                            aft.append(MsgArray[l]);
-                        }
-                    }
-                }
-                MessageChain chain = new MessageChainBuilder()
-                        .append(prev.toString())
-                        .append(new Face(Integer.parseInt(num.toString())))
-                        .append(aft)
-                        .append(new At(msgEvent.getSender().getId()))
-                        .build();
-                msgEvent.getSubject().sendMessage(chain);
-            }else {
-                MessageChain chain=new MessageChainBuilder()
-                        .append(result)
-                        .append(new At(msgEvent.getSender().getId()))
-                        .build();
-            }
+            MessageChain msg = msgEvent.getMessage();
+            String result = Client.getReply(msgProcess(msg));
+            MessageChain chain=new MessageChainBuilder()
+                    .append(result)
+                    .append(new At(msgEvent.getSender().getId()))
+                    .build();
+            msgEvent.getGroup().sendMessage(chain);
             Logger logger=Logger.getDeclaredLogger();
             logger.info("Handled chatting event from Group: %s",msgEvent.getGroup().getId());
         }
@@ -78,43 +45,22 @@ public class Replier extends SimpleListenerHost {
     }
     @EventHandler
     public void privateTalk(FriendMessageEvent msgEvent) throws IOException, LoggerNotDeclaredException {
-        String msg = msgEvent.getMessage().contentToString();
-        msg = msg.replace(" ", "%20");
-        String result = getApi("https://api.qingyunke.com/api.php?key=free&appid=0&msg=" + msg, "content");
-        result = result.replace("菲菲", msgEvent.getBot().getNick());
-        if (result.contains("{") && result.contains("}")) {
-            StringBuilder prev = new StringBuilder();
-            StringBuilder aft = new StringBuilder();
-            StringBuilder num = new StringBuilder();
-            char[] MsgArray = result.toCharArray();
-            for (int i = 0; MsgArray[i] != '{'; i++) {
-                prev.append(MsgArray[i]);
-            }
-            for (int j = 1; j < MsgArray.length; j++) {
-                if (MsgArray[j - 1] == ':') {
-                    for (int k = j; MsgArray[k + 1] != '}'; k++) {
-                        num.append(MsgArray[k]);
-                    }
-                }
-                if (MsgArray[j - 1] == '}') {
-                    for (int l = j; l < MsgArray.length; l++) {
-                        aft.append(MsgArray[l]);
-                    }
-                }
-            }
-            MessageChain chain = new MessageChainBuilder()
-                    .append(prev.toString())
-                    .append(new Face(Integer.parseInt(num.toString())))
-                    .append(aft)
-                    .build();
-            msgEvent.getSubject().sendMessage(chain);
-        }else {
-            MessageChain chain=new MessageChainBuilder()
-                    .append(result)
-                    .build();
-        }
+        MessageChain msg = msgEvent.getMessage();
+        String result = Client.getReply(msgProcess(msg));
+        msgEvent.getSubject().sendMessage(result);
         Logger logger=Logger.getDeclaredLogger();
         logger.info("Handled message reply at"+msgEvent.getSubject().getId());
+    }
+
+    public static String msgProcess(MessageChain msg){
+        StringBuilder textMessage = new StringBuilder("");
+        for (SingleMessage messageContent : msg) {
+            if (messageContent instanceof PlainText) {
+                PlainText plainText = (PlainText) messageContent;
+                textMessage.append(plainText.getContent());
+            }
+        }
+        return textMessage.toString();
     }
 
 }
---
+++ src/main/java/io/github/et/tools/Client.java
@@ -0,0 +1,27 @@
+package io.github.et.tools;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    private static final Socket socket;
+    private static OutputStream outputStream;
+    private static InputStream inputStream;
+    private static PrintWriter writer;
+    private static BufferedReader reader;
+    static{
+        try {
+            socket = new Socket("127.0.0.1", 35386);
+            outputStream = socket.getOutputStream();
+            writer = new PrintWriter(outputStream, true);
+            inputStream = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static String getReply(String msg) throws IOException {
+        writer.println("嘿"+msg);
+        return reader.readLine();
+    }
+}
